96e124857e604c201135d58c7182d22cbcd842bc
Andrei <skibin-03@mail.ru>
2023-09-14 23:36:30 +0500

    Removed redundant code

--- handlers/client/common.py
+++ handlers/client/common.py
@@ -11,22 +11,6 @@ from keyboards.constants import START_MESSAGE, HELP_MESSAGE, MENU_KEYBOARD
 router = Router()
 
 
-# @router.message(Command("найти"))
-# @get_accessor(WorkersAccessor)
-# async def search(message: Message, state: FSMContext, accessor: WorkersAccessor) -> None:
-#     await message.answer(chat_id=message.chat.id)
-#     await state.set_state(FindWorker.waiting_for_data)
-
-
-# @router.message(FindWorker.waiting_for_data)
-# @get_accessor(WorkersAccessor)
-# async def help_msg(message: Message, state: FSMContext, accessor: WorkersAccessor):
-#     data = message.text.split()
-#     if len(data) == 2:
-#         res = accessor.get(name=data[0],surname=data[1])
-#     await message.answer(text=HELP_MESSAGE)
-
-
 @router.callback_query(F.data == "no_data")
 async def common_callback(message: CallbackQuery):
     await message.answer()
